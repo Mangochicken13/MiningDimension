af2eaa1626a4035aaba7b15b36c84fa622f9ab7e
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2023-09-28 13:48:40 +0800

    Update MiningSubworldID.cs
    Add Count and Search fields to MiningSubworldID

--- MiningSubworldID.cs
+++ MiningSubworldID.cs
@@ -1,14 +1,21 @@
-﻿
+﻿using ReLogic.Reflection;
+
 namespace MiningDimension
 {
+    // Name for these can be finnicky, probably also want to add some sort of search function like the tmod ID classes
+    // I choose byte because i don't think there will ever be more than 255 subworlds (but then again vanilla thought that about wings lol)
     public class MiningSubworldID
     {
-        public const int none = 0;
-        public const int PreBoss = 1;
-        public const int EvilOres = 2;
-        public const int Hell = 3;
-        public const int Hardmode = 4;
-        public const int Jungle = 5;
-        public const int Luminite = 6; // to-do: finalize tiers and names
+        public const byte none = 0;
+        public const byte PreBoss = 1;
+        public const byte EvilOres = 2;
+        public const byte Hell = 3;
+        public const byte Hardmode = 4;
+        public const byte Jungle = 5;
+        public const byte Luminite = 6;
+
+        public static readonly byte Count = 7;
+
+        public static readonly IdDictionary Search = IdDictionary.Create<MiningSubworldID, byte>();
     }
 }
